380d1f10479fa8cf6eaf81bb411358f2a0aaa688
Bernard Teye <datssosh@gmail.com>
2025-06-17 20:48:48 +0000

    move Navbar component from Home component into RootLayout component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import { Container } from "@/components/atoms/grid/Grid";
-import Navbar from "@/components/organisms/navbar/Navbar";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
       <Container>
         <h1>Getting started</h1>
       </Container>
