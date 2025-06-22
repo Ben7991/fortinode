da94ea0a90b49aba012157ad4b53047984d4bb04
Bernard Teye <datssosh@gmail.com>
2025-06-22 07:42:07 +0000

    refactor Container and Row component to eliminate undefined from showing up in the rendered dom

--- components/atoms/grid/Grid.tsx
+++ components/atoms/grid/Grid.tsx
@@ -1,11 +1,13 @@
-import { ComponentPropsWithoutRef } from "react";
+import { ComponentPropsWithoutRef } from 'react';
 
-export function Container(props: ComponentPropsWithoutRef<"div">) {
+export function Container(props: ComponentPropsWithoutRef<'div'>) {
   const { className, ...remainingProps } = props;
 
   return (
     <div
-      className={`${className} w-[90%] md:w-[85%] xl:w-[80%] 2xl:w-[1135px] m-auto`}
+      className={`${
+        className ?? ''
+      } w-[90%] md:w-[85%] xl:w-[80%] 2xl:w-[1135px] m-auto`}
       {...remainingProps}
     >
       {props.children}
@@ -13,11 +15,11 @@ export function Container(props: ComponentPropsWithoutRef<"div">) {
   );
 }
 
-export function Row(props: ComponentPropsWithoutRef<"div">) {
+export function Row(props: ComponentPropsWithoutRef<'div'>) {
   const { className, ...remainingProps } = props;
 
   return (
-    <div className={`${className} flex`} {...remainingProps}>
+    <div className={`${className ?? ''} flex`} {...remainingProps}>
       {props.children}
     </div>
   );
