e13814b5c98c2d8ee9b70d6833f42b219b9f4bd8
Bernard Teye <datssosh@gmail.com>
2025-07-20 19:08:05 +0000

    add optional className prop to SectionHeading component and make use in ContactForm component

--- components/molecules/section-heading/SectionHeading.tsx
+++ components/molecules/section-heading/SectionHeading.tsx
@@ -2,13 +2,17 @@ import Headline from '@/components/atoms/headline/Headline'
 
 type SectionHeadingProps = {
   text: string
+  className?: string
 }
 
-export default function SectionHeading({ text }: SectionHeadingProps) {
+export default function SectionHeading({
+  text,
+  className,
+}: SectionHeadingProps) {
   return (
     <div className="relative inline-block">
       <div className="absolute w-12 h-12 rounded-full bg-[var(--light-blue-100)] -z-[1] -top-2 -left-3" />
-      <Headline tag="h3" className="mb-3">
+      <Headline tag="h3" className={`mb-3 ${className}`}>
         {text}
       </Headline>
     </div>
--- components/organisms/contact-form/ContactForm.tsx
+++ components/organisms/contact-form/ContactForm.tsx
@@ -4,16 +4,33 @@ import { SubmitHandler, useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 
 import { Col, Container, Row } from '@/components/atoms/grid/Grid'
-import Headline from '@/components/atoms/headline/Headline'
-import { FormControl, FormControlError, FormGroup, FormLabel } from '@/components/atoms/form/Form'
-import { PiEnvelope, PiNotePencil, PiPaperPlaneRight, PiUser } from 'react-icons/pi'
+import {
+  FormControl,
+  FormControlError,
+  FormGroup,
+  FormLabel,
+} from '@/components/atoms/form/Form'
+import {
+  PiEnvelope,
+  PiNotePencil,
+  PiPaperPlaneRight,
+  PiUser,
+} from 'react-icons/pi'
 import Button from '@/components/atoms/button/Button'
-import { ContactFormInputs, contactFormSchema } from '@/components/organisms/contact-form/ContactForm.utils'
+import {
+  ContactFormInputs,
+  contactFormSchema,
+} from '@/components/organisms/contact-form/ContactForm.utils'
+import SectionHeading from '@/components/molecules/section-heading/SectionHeading'
 
 export default function ContactForm() {
-  const { register, handleSubmit, formState: { errors } } = useForm<ContactFormInputs>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ContactFormInputs>({
     resolver: yupResolver(contactFormSchema),
-    mode: 'onBlur'
+    mode: 'onBlur',
   })
 
   const onSubmit: SubmitHandler<ContactFormInputs> = (data) => {
@@ -23,42 +40,81 @@ export default function ContactForm() {
   return (
     <article className="pb-15 md:pb-10 xl:pb-15">
       <Container>
-        <Headline tag="h3" className="mb-5 heading-semibold">Or submit the form below</Headline>
+        <SectionHeading
+          text="Or submit the form below"
+          className="heading-semibold"
+        />
         <form onSubmit={handleSubmit(onSubmit)}>
           <Row className="flex-col md:flex-row justify-between gap-4 mb-5">
             <Col colSize="col-4">
               <FormGroup>
                 <FormLabel>Name</FormLabel>
-                <FormControl el="input" icon={<PiUser className="text-xl" />} placeholder="Enter your name"
-                             type="name" {...register('name')} hasError={Boolean(errors.name)} />
-                {Boolean(errors.name) && <FormControlError message={errors.name?.message} />}
+                <FormControl
+                  el="input"
+                  icon={<PiUser className="text-xl" />}
+                  placeholder="Enter your name"
+                  type="name"
+                  {...register('name')}
+                  hasError={Boolean(errors.name)}
+                />
+                {Boolean(errors.name) && (
+                  <FormControlError message={errors.name?.message} />
+                )}
               </FormGroup>
             </Col>
             <Col colSize="col-4">
               <FormGroup>
                 <FormLabel>Email</FormLabel>
-                <FormControl el="input" icon={<PiEnvelope className="text-xl" />} placeholder="Enter your email"
-                             type="email" {...register('email')} hasError={Boolean(errors.email)}/>
-                {Boolean(errors.email) && <FormControlError message={errors.email?.message} />}
+                <FormControl
+                  el="input"
+                  icon={<PiEnvelope className="text-xl" />}
+                  placeholder="Enter your email"
+                  type="email"
+                  {...register('email')}
+                  hasError={Boolean(errors.email)}
+                />
+                {Boolean(errors.email) && (
+                  <FormControlError message={errors.email?.message} />
+                )}
               </FormGroup>
             </Col>
             <Col colSize="col-4">
               <FormGroup>
                 <FormLabel>Subject</FormLabel>
-                <FormControl el="input" icon={<PiNotePencil className="text-xl" />} placeholder="Enter your subject"
-                             type="text" {...register('subject')} hasError={Boolean(errors.subject)}/>
-                {Boolean(errors.subject) && <FormControlError message={errors.subject?.message} />}
+                <FormControl
+                  el="input"
+                  icon={<PiNotePencil className="text-xl" />}
+                  placeholder="Enter your subject"
+                  type="text"
+                  {...register('subject')}
+                  hasError={Boolean(errors.subject)}
+                />
+                {Boolean(errors.subject) && (
+                  <FormControlError message={errors.subject?.message} />
+                )}
               </FormGroup>
             </Col>
           </Row>
           <Col colSize="col-12" className="mb-6">
             <FormGroup>
               <FormLabel>Message</FormLabel>
-              <FormControl el="textarea" placeholder="Enter your message" rows={6} {...register('description')} hasError={Boolean(errors.description)} />
-              {Boolean(errors.description) && <FormControlError message={errors.description?.message} />}
+              <FormControl
+                el="textarea"
+                placeholder="Enter your message"
+                rows={6}
+                {...register('description')}
+                hasError={Boolean(errors.description)}
+              />
+              {Boolean(errors.description) && (
+                <FormControlError message={errors.description?.message} />
+              )}
             </FormGroup>
           </Col>
-          <Button el="button" variant="primary" className="px-5 flex items-center justify-center gap-2 cursor-pointer">
+          <Button
+            el="button"
+            variant="primary"
+            className="px-5 flex items-center justify-center gap-2 cursor-pointer"
+          >
             Submit <PiPaperPlaneRight className="text-xl" />
           </Button>
         </form>
