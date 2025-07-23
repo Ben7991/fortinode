760f1ca1288748b1f6e07075553491541f3aa86f
Bernard Teye <datssosh@gmail.com>
2025-07-23 05:18:40 +0000

    move footer containers into Footer.partials to create BrandDetails, ContactDetails, QuickLinks, and Copyrights component

---
+++ components/organisms/footer/Footer.partials.tsx
@@ -0,0 +1,118 @@
+import Link from 'next/link'
+import { FiMail, FiPhone } from 'react-icons/fi'
+import { GrShare } from 'react-icons/gr'
+
+import { Col, Container, Row } from '@/components/atoms/grid/Grid'
+import Headline from '@/components/atoms/headline/Headline'
+import AppLogo from '@/components/molecules/app-logo/AppLogo'
+
+export function BrandDetails() {
+  return (
+    <Col colSize="col-4">
+      <div className="bg-white py-2 px-3 rounded-md lg:w-2/3">
+        <AppLogo />
+      </div>
+      <p className="mt-4">
+        Based in Ghana, we are deeply committed to empowering businesses,
+        especially Small and Medium-sized Enterprises (SMEs), with robust,
+        accessible, and effective digital protection and IT support.
+      </p>
+    </Col>
+  )
+}
+
+export function ContactDetails() {
+  return (
+    <Col colSize="col-4">
+      <div>
+        <Headline tag="h5" className="text-[#fff!important] mb-2">
+          Our Contact Info
+        </Headline>
+        <Link
+          href="tel:+233531937928"
+          className="mb-2 text-[0.875em] flex items-center gap-2 hover:underline"
+        >
+          <FiPhone className="text-xl" /> +233 53 193 7928
+        </Link>
+        <Link
+          href="mailto: cybersive.solutions@gmail.com"
+          className="text-[0.875em] flex items-center gap-2 hover:underline"
+        >
+          <FiMail className="text-xl" /> cybersive.solutions@gmail.com
+        </Link>
+      </div>
+    </Col>
+  )
+}
+
+export function QuickLinks() {
+  return (
+    <Col colSize="col-4">
+      <Headline tag="h5" className="mb-2 text-[#fff!important] md:text-center">
+        Quick Links
+      </Headline>
+      <div className="flex flex-wrap gap-4 md:flex-col md:items-center md:gap-2">
+        <Link href="/" className="hover:text-white text-[0.875em]">
+          Home
+        </Link>
+        <Link href="/about-us" className="hover:text-white text-[0.875em]">
+          About Us
+        </Link>
+        <Link
+          href="/prevention-hardening"
+          className="hover:text-white text-[0.875em]"
+        >
+          Proactive Prevention & Hardening
+        </Link>
+        <Link
+          href="/prevention-hardening"
+          className="hover:text-white text-[0.875em]"
+        >
+          Detection & Monitoring
+        </Link>
+        <Link
+          href="/response-recovery"
+          className="hover:text-white text-[0.875em]"
+        >
+          Response & Recovery
+        </Link>
+        <Link
+          href="/cybersecurity-training"
+          className="hover:text-white text-[0.875em]"
+        >
+          Cybersecurity Training
+        </Link>
+        <Link
+          href="/managed-services"
+          className="hover:text-white text-[0.875em]"
+        >
+          Managed IT Services
+        </Link>
+        <Link href="/contact-us" className="hover:text-white text-[0.875em]">
+          Contact Us
+        </Link>
+      </div>
+    </Col>
+  )
+}
+
+export function Copyrights() {
+  return (
+    <Container className="mt-10 border-t border-t-white pt-10">
+      <Row className="flex-col items-center gap-4 md:flex-row md:justify-between md:items-center">
+        <p className="text-center md:text-start">
+          Copyright &copy; 2025 Cybersive Solutions | All rights reserved
+        </p>
+        <Col colSize="col-4" className="flex justify-end">
+          <Link
+            href="/"
+            className="inline-block hover:text-white"
+            target="_blank"
+          >
+            Powered By Bernard Teye <GrShare className="inline-block" />
+          </Link>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
--- components/organisms/footer/Footer.tsx
+++ components/organisms/footer/Footer.tsx
@@ -1,94 +1,22 @@
-import Link from 'next/link'
-import { GrShare } from 'react-icons/gr'
-import { FiMail, FiPhone } from 'react-icons/fi'
-
-import { Col, Container, Row } from '@/components/atoms/grid/Grid'
-import Headline from '@/components/atoms/headline/Headline'
-import AppLogo from '@/components/molecules/app-logo/AppLogo'
+import { Container, Row } from '@/components/atoms/grid/Grid'
+import {
+  BrandDetails,
+  ContactDetails,
+  Copyrights,
+  QuickLinks,
+} from './Footer.partials'
 
 export default function Footer() {
   return (
     <footer className="bg-[var(--dark-blue-200)] text-gray-400 py-5 md:py-10 xl:py-15">
       <Container>
         <Row className="flex-col gap-6 md:flex-row md:gap-0">
-          <Col colSize="col-4">
-            <div className="bg-white py-2 px-3 rounded-md lg:w-2/3">
-              <AppLogo />
-            </div>
-            <p className="mt-4">
-              Based in Ghana, we are deeply committed to empowering businesses,
-              especially Small and Medium-sized Enterprises (SMEs), with robust,
-              accessible, and effective digital protection and IT support.
-            </p>
-          </Col>
-          <Col colSize="col-4">
-            <Headline
-              tag="h5"
-              className="mb-2 text-[#fff!important] md:text-center"
-            >
-              Quick Links
-            </Headline>
-            <div className="flex flex-wrap gap-4 md:flex-col md:items-center md:gap-2">
-              <Link href="/" className="hover:text-white text-[0.875em]">
-                Home
-              </Link>
-              <Link
-                href="/about-us"
-                className="hover:text-white text-[0.875em]"
-              >
-                About Us
-              </Link>
-              <Link
-                href="/services"
-                className="hover:text-white text-[0.875em]"
-              >
-                Services
-              </Link>
-              <Link
-                href="/contact-us"
-                className="hover:text-white text-[0.875em]"
-              >
-                Contact Us
-              </Link>
-            </div>
-          </Col>
-          <Col colSize="col-4">
-            <div>
-              <Headline tag="h5" className="text-[#fff!important] mb-2">
-                Our Contact Info
-              </Headline>
-              <Link
-                href="tel:+233531937928"
-                className="mb-2 text-[0.875em] flex items-center gap-2 hover:underline"
-              >
-                <FiPhone className="text-xl" /> +233 53 193 7928
-              </Link>
-              <Link
-                href="mailto: cybersive.solutions@gmail.com"
-                className="text-[0.875em] flex items-center gap-2 hover:underline"
-              >
-                <FiMail className="text-xl" /> cybersive.solutions@gmail.com
-              </Link>
-            </div>
-          </Col>
+          <BrandDetails />
+          <QuickLinks />
+          <ContactDetails />
         </Row>
       </Container>
-      <Container className="mt-10 border-t border-t-white pt-10">
-        <Row className="flex-col items-center gap-4 md:flex-row md:justify-between md:items-center">
-          <p className="text-center md:text-start">
-            Copyright &copy; 2025 Cybersive Solutions | All rights reserved
-          </p>
-          <Col colSize="col-4" className="flex justify-end">
-            <Link
-              href="/"
-              className="inline-block hover:text-white"
-              target="_blank"
-            >
-              Powered By Bernard Teye <GrShare className="inline-block" />
-            </Link>
-          </Col>
-        </Row>
-      </Container>
+      <Copyrights />
     </footer>
   )
 }
